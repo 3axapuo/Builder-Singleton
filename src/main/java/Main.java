7e6542594b576d7d1df9aca63452e8a8e278d924
3axapuo <3axapuo@mail.ru>
2022-10-21 00:43:08 +0300

    Task No. 1 is completed
    - реализованна практика с шаблоном Builder (Строитель).

---
+++ src/main/java/Main.java
@@ -0,0 +1,44 @@
+public class Main {
+    public static void main(String[] args) {
+        Person mom = new PersonBuilder()
+                .setName("Анна")
+                .setSurname("Вольф")
+                .setAge(31)
+                .setAddress("Сидней")
+                .build();
+        mom.happyBirthday(); //день рождения у mom
+        mom.getAge();
+        mom.hashCode();
+        Person son = mom.newChildBuilder()
+                .setName("Антошка")
+                .build();
+        System.out.println("У " + mom + " есть сын, " + son);
+
+        try {
+            // Не хватает обязательных полей
+            Person brother = new PersonBuilder()
+                    .setName("Данила")
+                    .setAddress("Москва")
+                    .setAge(2)
+                    .build();
+            System.out.println("У " + mom + " есть родной брат, " + brother);
+            //new PersonBuilder().build();
+        } catch (IllegalStateException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            // Возраст недопустимый
+            Person father = new PersonBuilder()
+                    .setName("Дмитрий")
+                    .setSurname("Вольф")
+                    .setAddress("Сидней")
+                    .setAge(-100)
+                    .build();
+            System.out.println("У " + mom + " есть муж, " + father);
+            //new PersonBuilder().setAge(-100).build();
+        } catch (IllegalArgumentException e) {
+            e.printStackTrace();
+        }
+    }
+}
